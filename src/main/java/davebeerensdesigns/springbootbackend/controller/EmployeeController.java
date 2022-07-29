e17ba63286e43fc705ac68a9dea0a230207e34dd
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-29 13:28:03 +0200

    get employee by id endpoint

--- src/main/java/davebeerensdesigns/springbootbackend/controller/EmployeeController.java
+++ src/main/java/davebeerensdesigns/springbootbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package davebeerensdesigns.springbootbackend.controller;
 
+import davebeerensdesigns.springbootbackend.exception.ResourceNotFoundException;
 import davebeerensdesigns.springbootbackend.model.Employee;
 import davebeerensdesigns.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,12 @@ public class EmployeeController {
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    // build get employee by id REST API
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee doest not exist with id:" + id));
+        return ResponseEntity.ok(employee);
     }
 }
