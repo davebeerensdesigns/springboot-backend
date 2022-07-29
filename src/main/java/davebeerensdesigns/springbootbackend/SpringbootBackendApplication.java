6e8baead97739388f96390b919f64ca4043200e8
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-29 10:14:38 +0200

    exposing employee to rest api

--- src/main/java/davebeerensdesigns/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/davebeerensdesigns/springbootbackend/SpringbootBackendApplication.java
@@ -1,14 +1,36 @@
 package davebeerensdesigns.springbootbackend;
 
+import davebeerensdesigns.springbootbackend.model.Employee;
+import davebeerensdesigns.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBackendApplication.class, args);
         System.out.println("Application started");
-    }
 
+
+    }
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public void run(String... args) throws Exception{
+        Employee employee = new Employee();
+        employee.setFirstName("John");
+        employee.setLastName("Doe");
+        employee.setEmailId("john@gmail.com");
+        employeeRepository.save(employee);
+
+        Employee employee1 = new Employee();
+        employee1.setFirstName("Jane");
+        employee1.setLastName("Doe");
+        employee1.setEmailId("jane@gmail.com");
+        employeeRepository.save(employee1);
+    }
 }
