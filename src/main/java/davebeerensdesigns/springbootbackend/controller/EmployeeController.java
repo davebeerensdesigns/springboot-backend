62e0138d6f5bd2380bf8231358dd354f39f03f08
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-07-29 13:54:38 +0200

    update employee api endpoint

--- src/main/java/davebeerensdesigns/springbootbackend/controller/EmployeeController.java
+++ src/main/java/davebeerensdesigns/springbootbackend/controller/EmployeeController.java
@@ -34,4 +34,15 @@ public class EmployeeController {
         Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee doest not exist with id:" + id));
         return ResponseEntity.ok(employee);
     }
+
+    // build update employee REST API
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employee){
+        Employee employee1 = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee doest not exist with id:" + id));
+        employee1.setFirstName(employee.getFirstName());
+        employee1.setLastName(employee.getLastName());
+        employee1.setEmailId(employee.getEmailId());
+        Employee updatedEmployee = employeeRepository.save(employee1);
+        return ResponseEntity.ok(updatedEmployee);
+    }
 }
